88d52ccf47067fbfcda6a8f2f4f7c0de8bd1d7f7
Ospanova Mariyana <MTOspanova@beeline.kz>
2023-05-20 22:39:48 +0600

    create h1 element

--- main.js
+++ main.js
@@ -11,11 +11,13 @@ let wrap = document.querySelector(".wrapper")
 
 function addCard(block) {
     block.forEach(val => {
-        let div = document.createElement('div')
+        let div = document.createElement('div');
 
         let img = document.createElement('img');
         img.src = val.image;
 
+        let h1 = document.createElement('h1');
+        h1.innerText = val.name; 
 
         div.append(img)
     })
